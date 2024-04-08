02f09289dfa71028d6ffc86cecc2bb56440ed8b6
Saugatkafley <saugatkafley@gmail.com>
2024-04-08 21:41:11 +0545

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+"API test for Groq"
+import os
+from dotenv import load_dotenv
+
+from groq import Groq
+
+load_dotenv()
+client = Groq(api_key=os.environ["GROQ_API_KEY"])
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of low latency LLMs",
+        }
+    ],
+    model="mixtral-8x7b-32768",
+)
+print(chat_completion.choices[0].message.content)
