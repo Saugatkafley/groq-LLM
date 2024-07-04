b448120cb897b6d2e85868666ef413e124f6574c
Saugatkafley <saugatkafley@gmail.com>
2024-07-04 09:06:47 +0545

    feat : add wikipedia tools

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # gradio app for groq-mistral langgraph demo:
 import time
 from dotenv import load_dotenv
+from PIL import Image
 
 load_dotenv()
 
@@ -9,8 +10,26 @@ from utils import convert_to_agent_state
 import gradio as gr
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
+from tools import WikiInputs
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
 
-SYSTEM_PROMPT = "You are a helpful assistant."
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+tool = WikipediaQueryRun(
+    name="wiki-tool",
+    description="look up things in wikipedia",
+    args_schema=WikiInputs,
+    api_wrapper=api_wrapper,
+    return_direct=True,
+)
+
+SYSTEM_PROMPT = """You are a smart research assistant that is specialized on solving complex Mechanical Engineering problems. Use the search engine from Wikipedia  to look up information. \
+You are allowed to make multiple calls (either together or in sequence). \
+Only look up information when you are sure of what you want. \
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+"""
 MODEL_NAMES = [
     "llama3-70b-8192",
     "llama3-8b-8192",
@@ -23,8 +42,8 @@ def get_streaming_response(
     model_name: str, message_content: str, chat_history: list = []
 ):
     model = ChatGroq(model=model_name, temperature=0)  # reduce inference cost
-    agent = Agent(model, system=SYSTEM_PROMPT)
-    agent_states = convert_to_agent_state(chat_history)
+    agent = Agent(model, tools=[tool], system=SYSTEM_PROMPT)
+    agent_states = convert_to_agent_state(SYSTEM_PROMPT, chat_history)
 
     response = agent.graph.invoke(
         {"messages": agent_states + [HumanMessage(content=message_content)]}
@@ -40,12 +59,21 @@ def get_streaming_response(
         yield chat_history
 
 
+def draw_agent_graph():
+    return gr.Image(
+        "graph.png", type="filepath", label="Agent Graph", interactive=False
+    )
+    # return Image.open(
+    #     Agent(model_name).graph.get_graph().draw_png(output_file_path="graph.png")
+    # )
+
+
 with gr.Blocks(theme="soft") as demo:
     gr.Markdown(
         value="""
         <h1 align="center">Lang-graph Agent Groq</h1>
-        <h4 align="center">Chatbot Demo</h4>
-        <h3 align="center">Powered by Langchain and Groq</h3>
+        <h4 align="center">Mechanical Chatbot Demo</h4>
+        <h3 align="center">Powered by LangGraph,Groq and Wikipedia</h3>
         """
     )
     model_name = gr.Dropdown(
@@ -54,6 +82,7 @@ with gr.Blocks(theme="soft") as demo:
         label="Model Name",
         interactive=True,
     )
+    system = gr.Textbox(value=SYSTEM_PROMPT, interactive=False, label="System Prompt")
     chatbot = gr.Chatbot()
     message = gr.Textbox()
     submit = gr.Button("Submit", variant="primary")
@@ -65,4 +94,9 @@ with gr.Blocks(theme="soft") as demo:
     )
     clear = gr.ClearButton()
     clear.click(lambda: [], None, chatbot, queue=False)
+
+    with gr.Row():
+        generate_graph = gr.Button("Get Agent Graph", variant="secondary")
+        image_graph = gr.Image()
+        generate_graph.click(draw_agent_graph, None, outputs=[image_graph])
 demo.launch()
