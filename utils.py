3e5f0aa70d792b7be1f28b9376627b0cd3890684
Saugatkafley <saugatkafley@gmail.com>
2024-07-05 12:49:46 +0545

    feat : utils to extract py code

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import os
+import re
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 
@@ -12,3 +14,26 @@ def convert_to_agent_state(SYSTEM_PROMPT, chat_history):
             agent_states.append(AIMessage(content=chat_history[i][1]))
 
     return agent_states
+
+
+def extract_python_code(text, output_directory="files", filename="python_code.py"):
+    # Extract Python code from text
+    print("text:", text)
+    pattern = r"```(.*?)```"
+    matches = re.findall(pattern, text, re.DOTALL)
+    print("matches:", matches)
+
+    if matches:
+        python_code = matches[0].strip()
+        # Ensure the output directory exists
+        os.makedirs(output_directory, exist_ok=True)
+        # Define the complete file path
+        file_path = os.path.join(output_directory, filename)
+        # Write the extracted code to the file
+        with open(file_path, "w") as file:
+            file.write(python_code)
+        print(f"Python code saved to {file_path}")
+        return python_code , file_path
+    else:
+        print("No Python code found")
+        return None
