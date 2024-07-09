26e4ed27e81cc14c0e2deae16886559ada82b408
Saugatkafley <saugatkafley@gmail.com>
2024-07-09 13:42:16 +0545

    small changes

--- tools.py
+++ tools.py
@@ -7,3 +7,11 @@ class WikiInputs(BaseModel):
     query: str = Field(
         description="query to look up in Wikipedia, should be 3 or less words"
     )
+
+
+class RAGInputs(BaseModel):
+    """Inputs to the RAG tool."""
+
+    query: str = Field(
+        description="Use the context from the documents to answer the question. Use the best context.Try to be as specific as possible. Do not make up answers."
+    )
